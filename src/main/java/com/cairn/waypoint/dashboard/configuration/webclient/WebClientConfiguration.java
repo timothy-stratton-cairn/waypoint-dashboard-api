c8887fc094ea93521b00a29b0c26cfd9422f523b
timothy.stratton <tim@cairnfg.com>
2024-04-19 15:06:17 -0500

    Updating the WebClient url

--- src/main/java/com/cairn/waypoint/dashboard/configuration/webclient/WebClientConfiguration.java
+++ src/main/java/com/cairn/waypoint/dashboard/configuration/webclient/WebClientConfiguration.java
@@ -1,5 +1,6 @@
 package com.cairn.waypoint.dashboard.configuration.webclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,8 +8,11 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfiguration {
 
+  @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+  private String authServerUri;
+
   @Bean(name = "authorizationApiWebClient")
   public WebClient authorizationApiWebClient() {
-    return WebClient.create("http://localhost:8082");
+    return WebClient.create(authServerUri);
   }
 }
