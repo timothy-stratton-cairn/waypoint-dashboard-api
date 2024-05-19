27b8b0a9a269a5d585bab944c6848f01fbe7522a
timothy.stratton <tim@cairnfg.com>
2024-05-18 22:24:23 -0500

    Updating Swagger Tags

--- src/main/java/com/cairn/waypoint/dashboard/endpoints/questiontype/GetAllQuestionTypesEndpoint.java
+++ src/main/java/com/cairn/waypoint/dashboard/endpoints/questiontype/GetAllQuestionTypesEndpoint.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@Tag(name = "Protocol Step Status")
+@Tag(name = "Homework Question Type")
 public class GetAllQuestionTypesEndpoint {
 
   public static final String PATH = "/api/homework-question/type";
